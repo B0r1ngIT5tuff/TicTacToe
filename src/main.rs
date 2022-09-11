002854316b5dd88ab37d32040dfe3cf04911b6a2
Davide Di Nardo <dvdinardo.15.38@gmail.com>
2022-09-11 15:22:58 +0200

    Minor fixes

--- src/main.rs
+++ src/main.rs
@@ -72,53 +72,53 @@ fn positioncheck(t: &mut [char]) -> bool{
     let mut continua: bool = true ;
 
     if t[0] == 'X' && t[1] == 'X' && t[2] == 'X' {          // P1 wins
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[3] == 'X' && t[4] == 'X' && t[5] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[6] == 'X' && t[7] == 'X' && t[8] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[0] == 'X' && t[3] == 'X' && t[6] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[1] == 'X' && t[4] == 'X' && t[7] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[2] == 'X' && t[5] == 'X' && t[8] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[0] == 'X' && t[4] == 'X' && t[8] == 'X' {
-        println!("Player 1 wins!!");
         continua = false;
+        println!("Player 1 wins!!");
     } else if t[2] == 'X' && t[4] == 'X' && t[6] == 'X' {
+        continua = false;
         println!("Player 1 wins!!");
-        continua = false;
     } else if t[0] == 'O' && t[1] == 'O' && t[2] == 'O' {   // P2 wins
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[3] == 'O' && t[4] == 'O' && t[5] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[6] == 'O' && t[7] == 'O' && t[8] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[0] == 'O' && t[3] == 'O' && t[6] == 'O'{
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[1] == 'O' && t[4] == 'O' && t[7] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[2] == 'O' && t[5] == 'O' && t[8] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[0] == 'O' && t[4] == 'O' && t[8] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     } else if t[2] == 'O' && t[4] == 'O' && t[6] == 'O' {
-        println!("Player 2 wins!!");
         continua = false;
+        println!("Player 2 wins!!");
     }
 
     continua
